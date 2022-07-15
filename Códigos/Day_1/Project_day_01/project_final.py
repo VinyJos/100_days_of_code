dd38918287aa0bc5583026e01945d1ce9a0f804a
Vinicius Jose <92282414+VinyJos@users.noreply.github.com>
2022-07-15 18:24:37 -0300

    Delete Códigos/Day_1/Project_day_01 directory

--- Códigos/Day_1/Project_day_01/project_final.py
+++
@@ -1,4 +0,0 @@
-print('Welcome to the Band Name Generator.')
-city = input("What's name of the city you grew up in?\n")
-name_pet = input("What's your pet's name?\n")
-print("Your band name is " + city + ' ' + name_pet) 
